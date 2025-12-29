05b0c7b036326dd46c7826e33466e5a7f8f16776
ratatatatcode <pogisijamesmagnaye@gmail.com>
2025-12-29 18:08:43 +0800

    Initial commit

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center text-center h-screen">
+      <h1 className="text-6xl font-bold">Hello World</h1>
+    </main>
+  );
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "React CSS Effects",
+  description:
+    "A personal exploration of CSS effects in React, built manually without using AI.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${poppins.variable} scroll-smooth antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
