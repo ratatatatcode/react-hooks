6ff5f027e2ce00921e5632f513a89fef52f52e8f
ratatatatcode <pogisijamesmagnaye@gmail.com>
2026-06-24 00:39:20 +0800

    Reset

--- src/app/(root)/components/page.tsx
+++
@@ -1,9 +0,0 @@
-import ExampleForm from '@/components/form/example-form';
-
-export default function Components() {
-  return (
-    <main className="flex min-h-screen w-full flex-col items-center text-center">
-      <ExampleForm />
-    </main>
-  );
-}
--- src/app/(root)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function MainLayout({ children }: { children: React.ReactNode }) {
-  return <div className="min-h-screen w-full antialiased">{children}</div>;
-}
