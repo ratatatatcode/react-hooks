24787fbcfb907ac2e873ac3ffddea23c7845735a
ratatatatcode <pogisijamesmagnaye@gmail.com>
2026-06-24 20:39:05 +0800

    Add useRef example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import UseStateComponent from "./components/hooks/useState";
 import UseEffectComponent from "./components/hooks/useEffect";
+import UseRefComponent from "./components/hooks/useRef";
 
 function App() {
   return (
     <div className="min-h-screen w-full flex flex-col items-center gap-4 p-4">
       <UseStateComponent />
       <UseEffectComponent />
+      <UseRefComponent />
     </div>
   );
 }
